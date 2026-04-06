0287f557f4a8f795a768b55303afef4d4a73c6ef
Javier Jaramillo ACR <cristian.cj99@gmail.com>
2026-04-05 19:01:06 -0500

    Continuacion de autorizacion y autenticacion

--- routers/autenticacion_basica.py
+++ routers/autenticacion_basica.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI , Depends
+from fastapi import FastAPI , Depends, HTTPException
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
@@ -42,14 +42,26 @@ users_db = {
 }
 
 
-def serch_user(username:str):
+def search_user(username:str):
     if username in users_db:
         return UserDB(users_db[username])
 
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
+    user_db = users_db.get(form.username)
+    if not user_db:
+        raise HTTPException(status_code=400,detail="El usuario NO existe, pendejo")
+    user = search_user(form.username)
+    if not form.password == user.password:
+        raise HTTPException(status_code=400,detail="La contraseña no es correcta, vuelve a escribir")
     
-    
+    return {"access_token" : user.username, "token_type" : "bearer"}   
+
+@app.post("/users/me")
+async def me(user: User = Depends()):
+    return user
+
+
 
 
 
